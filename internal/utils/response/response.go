c53e59262bd05731032b049c9cdb09ebfc3bf9d7
Mackie <253263774@qq.com>
2023-03-28 09:55:36 +0800

    add response

---
+++ internal/utils/response/response.go
@@ -0,0 +1,28 @@
+package response
+
+// Resp 返回
+type Resp interface {
+	Ok() *Response
+	Fail() *Response
+}
+
+// Response 返回结构体
+type Response struct {
+	Code int         `json:"code" form:"code"` // 错误码 默认为0
+	Msg  string      `json:"msg" form:"msg"`   // 提示信息
+	Data interface{} `json:"data" form:"data"` // 内容
+}
+
+// Ok 成功返回
+func (r *Response) Ok(data interface{}) *Response {
+	r.Data = data
+	return r
+}
+
+// Fail 失败返回
+func (r *Response) Fail(code int, msg string, data interface{}) *Response {
+	r.Code = code
+	r.Msg = msg
+	r.Data = data
+	return r
+}
