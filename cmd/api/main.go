56e6d86a1c1cd6d72e63f5e8d5397ccef04fc49b
Mackie <253263774@qq.com>
2023-04-10 10:08:49 +0800

    add api

---
+++ cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	adminRouter "github.com/fennay/tgo/internal/app/admin/router"
+	"github.com/fennay/tgo/internal/utils/config"
+	"github.com/gin-gonic/gin"
+)
+
+// 入口文件
+func main() {
+	// 初始化配置
+	config.Init()
+	conf := config.GetConfig()
+
+	// 初始化 gin
+	g := gin.New()
+
+	// 加载路由
+	adminRouter.Init(g)
+
+	// 启动服务
+	url := fmt.Sprintf("%s:%d", conf.Http.URL, conf.Http.Port)
+	err := g.Run(url)
+	if err != nil {
+		return
+	}
+}
