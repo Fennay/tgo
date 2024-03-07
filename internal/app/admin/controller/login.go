4ebaf1132d05810577ce77c32f7d0ddec786b73f
Mackie <253263774@qq.com>
2024-03-07 22:07:33 +0800

    add login

--- internal/app/admin/controller/login.go
+++ internal/app/admin/controller/login.go
@@ -1,13 +1,16 @@
 package controller
 
 import (
+	"github.com/fennay/tgo/internal/utils/response"
 	"github.com/gin-gonic/gin"
 )
 
 func Login(c *gin.Context) {
-
+	response.Ok(c, "ok", nil)
+	return
 }
 
 func Logout(c *gin.Context) {
-
+	response.Ok(c, "ok", nil)
+	return
 }
--- internal/model/user/user.go
+++ internal/model/user/user.go
@@ -1,4 +1,5 @@
 package user
+
 import (
 	commonModel "github.com/fennay/tgo/internal/model"
 )
@@ -9,14 +10,14 @@ type User struct {
 	Username string `json:"username" form:"username"`
 	Nickname string `json:"nickname" form:"nickname"`
 	Password string `json:"password" form:"password"`
-	Phone string `json:"phone" form:"phone"`
-	Email string `json:"email" form:"email"`
-	Sex uint8 `json:"sex" form:"sex"`
-	Status uint8 `json:"status" form:"status"`
-	Sort int `json:"sort"`
+	Phone    string `json:"phone" form:"phone"`
+	Email    string `json:"email" form:"email"`
+	Sex      uint8  `json:"sex" form:"sex"`
+	Status   uint8  `json:"status" form:"status"`
+	Sort     int    `json:"sort"`
 }
 
 // TableName 设置表名称
 func (User) TableName() string {
-  return "user"
+	return "user"
 }
