dc09a0d26fefdefa911cb9a09f59ed7cb30b1bb9
Mackie <253263774@qq.com>
2024-03-09 15:34:51 +0800

    定义返回内容结构

--- internal/vo/resp/resp.go
+++ internal/vo/resp/resp.go
@@ -3,3 +3,16 @@ package resp
 type IDResp struct {
 	ID int `json:"id" form:"id"` // ID
 }
+
+// PageListResp 分页返回内容
+type PageListResp struct {
+	Pagination Pagination    `json:"pagination"` // 分页数据信息
+	List       []interface{} `json:"list"`       // 数据内容
+	Query      []interface{} `json:"query"`      // 查询参数
+}
+
+type Pagination struct {
+	Page     int `json:"page"`     // 当前页码
+	PageSize int `json:"pageSize"` // 每页数量
+	Total    int `json:"total"`    // 总数量
+}
