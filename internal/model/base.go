d9a99e1c92301ce8cd3211f1870f8cb7d3918aae
Mackie <253263774@qq.com>
2024-03-13 20:38:16 +0800

    添加基础model

--- internal/model/base.go
+++ internal/model/base.go
@@ -8,3 +8,15 @@ type Base struct {
 	CreatedAt time.Time `json:"createdAt" form:"createdAt"` // 创建时间
 	UpdatedAt time.Time `json:"updatedAt" form:"updatedAt"` // 更新时间
 }
+
+// OPEN_STATUS 开启状态
+const OPEN_STATUS = 1
+const CLOSE_STATUS = 0
+
+// DEAULT_SORT 默认排序
+const DEAULT_SORT = 999999
+
+type Status struct {
+	Status int `json:"status"` //
+	Sort   int `json:"sort"`   //
+}
