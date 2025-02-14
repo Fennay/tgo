5b36fc7b77adc35abd1b69aa1e55a897219c6004
Mackie <253263774@qq.com>
2025-02-14 16:40:37 +0800

    增加 session 中间件

--- internal/utils/strings/strings.go
+++ internal/utils/strings/strings.go
@@ -20,20 +20,20 @@ func init() {
 
 // Rand 自定义随机数
 func Rand(source string, n int) string {
-	return zyRand(source, n)
+	return dwRand(source, n)
 }
 
 // RandStringWithA2z 随机字符串 [A-z]
 func RandStringWithA2z(n int) string {
-	return zyRand(Letters, n)
+	return dwRand(Letters, n)
 }
 
 // RandNumRunes 随机获取数字
 func RandNumRunes(n int) string {
-	return zyRand(Num, n)
+	return dwRand(Num, n)
 }
 
-func zyRand(source string, n int) string {
+func dwRand(source string, n int) string {
 	lettersRune := []rune(source)
 	b := make([]rune, n)
 	for i := range b {
